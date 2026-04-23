b2d435401a6aa9a8d9c6face6d672176607387d1
tomoa75 <84170526+tomoa75@users.noreply.github.com>
2026-04-23 21:22:20 +0200

    dodana logika za dohvacanje sati po danima

--- src/components/HourlyForecast.jsx
+++ src/components/HourlyForecast.jsx
@@ -1,21 +1,66 @@
 import { getWeatherIcon } from "../getWeatherIcon";
+import { useState } from "react";
+
 export default function HourlyForecast({ hourly, units }) {
+  const weekDays = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
+
+  const todayIndex = new Date().getDay() - 1; // 0 = Sunday pa prilagodba da 0 bude Monday, 6 Sunday
+  console.log("todayIndex (0=Sun):", todayIndex);
+
+  const [offset, setOffset] = useState(todayIndex);
+  console.log("offset:", offset);
+
   if (!hourly || hourly.time.length === 0) {
     return <p>Hourly forecast data not available.</p>;
   }
-  // Uzmi samo prvih 24 sata iz onog dugačkog niza
-  const hourlyData = hourly.time.slice(0, 24).map((time, index) => {
-    return {
-      time: time,
-      temp: hourly.temperature_2m[index],
-      code: hourly.weathercode[index],
-    };
-  });
+
+  const handleChange = (e) => {
+    setOffset(Number(e.target.value));
+  };
+
+  //  KLJUČ: uvijek od danas + offset
+  const coef =
+    offset - todayIndex >= 0 ? offset - todayIndex : 7 + (offset - todayIndex);
+
+  const start = coef * 24;
+  const end = start + 24;
+  console.log(
+    "coef:",
+    coef,
+    "pomak:",
+    weekDays[offset],
+    "start:",
+    start,
+    "end:",
+    end,
+  );
+  const hourlyData = hourly.time.slice(start, end).map((time, i) => ({
+    time,
+    temp: hourly.temperature_2m[start + i],
+    code: hourly.weathercode[start + i],
+  }));
+
   return (
     <div className="hourly-forecast">
-      <h3>
-        Hourly Forecast <button>Refresh</button>
-      </h3>
+      <div className="hourly-forecast-header">
+        Hourly Forecast
+        <select value={offset} onChange={handleChange}>
+          {weekDays.map((day, i) => (
+            <option key={day} value={i}>
+              {day}
+            </option>
+          ))}
+        </select>
+      </div>
+
       <div className="hourly-forecast-list">
         {hourlyData.map((hour) => (
           <div key={hour.time} className="hourly-forecast-item">
